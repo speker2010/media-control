c48d1efa029895595f8915a95d0a0ca226c722ab
Artem Besedin <speker@mail.ru>
2021-07-26 23:15:53 +0400

    Update design

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
 <div class="player">
     <div class="control-panel">
-        <button class="control" @click="play">Play/Pause</button>
-        <button class="control" @click="mute">Mute/Unmute</button>
-        <button class="control" @click="prev">Prev</button>
-        <button class="control" @click="next">Next</button>
+        <button class="btn btn__secondary" @click="play">Play/Pause</button>
+        <button class="btn btn__secondary" @click="mute">Mute/Unmute</button>
+        <button class="btn btn__secondary" @click="prev">Prev</button>
+        <button class="btn btn__secondary" @click="next">Next</button>
     </div>
 </div>
 </template>
@@ -41,7 +41,14 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+
+$shadow: .3rem .3rem .6rem var(--greyLight-2), 
+-.2rem -.2rem .5rem var(--white);
+$inner-shadow: inset .2rem .2rem .5rem var(--greyLight-2), 
+inset -.2rem -.2rem .5rem var(--white);
+
 .player {
     display: flex;
     min-height: 100vh;
@@ -53,9 +60,44 @@ export default {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 20px;
+    border-radius: 12px;
+    box-shadow:.8rem .8rem 1.4rem var(--greyLight-2), 
+                -.2rem -.2rem 1.8rem var(--white);
+    padding: 4rem;
 }
-.control {
-    display: inline-block;
-    padding: 30px;
+
+.btn {
+    color: #000;
+    border: 0;
+    height: 4rem;
+    border-radius: 1rem;
+    box-shadow: $shadow;
+    cursor: pointer;
+    transition: .3s ease;
+
+    &__primary {
+        background: var(--primary);
+        box-shadow:inset .2rem .2rem 1rem var(--primary-light), 
+                inset -.2rem -.2rem 1rem var(--primary-dark),
+                $shadow;
+        color: var(--greyLight-1);
+        &:hover { color: var(--white); }
+        &:active {
+        box-shadow:inset .2rem .2rem 1rem var(--primary-dark), 
+                inset -.2rem -.2rem 1rem var(--primary-light);
+        }
+    }
+
+    &__secondary {
+        background: transparent;
+        &:hover { color: var(--primary); }
+        &:active {
+        box-shadow: $inner-shadow;
+        }
+    }
+
+    p { 
+        font-size: 1.6rem;
+    }
 }
 </style>
